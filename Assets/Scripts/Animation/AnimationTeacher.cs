c7d86a0ae615c53b3749b8453f02a53d5afae678
yesjessn <yelnguyen@ucdavis.edu>
2017-09-26 16:41:12 -0700

    AnimationTeacher

---
+++ Assets/Scripts/Animation/AnimationTeacher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTeacher : MonoBehaviour {
+
+	Animator childAnimator;
+
+	void Start () {
+		childAnimator = gameObject.GetComponent<Animator>();
+	}
+
+	void Update () {
+		if (!childAnimator.IsInTransition(0) && childAnimator.GetCurrentAnimatorStateInfo (0).normalizedTime >= 2.8) { //Average animation is 10.5s -> every 2.8 loops change for about 30s
+			var trigger = GetRandomTrigger ();
+			childAnimator.SetTrigger (trigger);
+			//print ("Trigger: " + trigger);
+		}
+		//print ("Normalized time: " + childAnimator.GetCurrentAnimatorStateInfo (0).normalizedTime);
+	}
+
+	private string GetRandomTrigger() {
+		var sample = Random.value;
+		if (sample < 0.5) {
+			return "Idle";
+		}
+		return "CrossArms";
+	}
+
+}
+
